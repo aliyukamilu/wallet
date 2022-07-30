a77e477121d5baa475452c76911e9055c0e11416
Aliyu Kamilu <aliyukamilu002@gmail.com>
2022-07-30 06:07:15 +0100

    added loader and response message

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,18 @@ const Home: NextPage = () => {
 
   const [balance, setBalance] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setSuccess] = useState(false);
 
   const testnet = "https://rinkeby.infura.io/v3/f3b0a7c03b7e47289cc277ed0d60579d"
   var web3 = new Web3(testnet);
 
-  var account = new Accounts(testnet);
+  // var account = new Accounts(testnet);
 
   const walletAddress = '0xc25e27911b334C6B854cC66DE4d7276A80881288';
   const privKey = '609cf9dd85af6380b683166c6d6448b79be6b36c25006f73f4a9c74688bdf676';
   const addressTo = '0x52EdB5dFCE4640644AD366b660ee9b78872b16c3'
 
-  if (typeof web3 !== 'undefined ') {
+  if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
   } else {
     web3 = new Web3(new Web3.providers.HttpProvider(testnet));
@@ -30,10 +31,16 @@ const Home: NextPage = () => {
       if (err) {
         console.log(err)
       } else {
-        setBalance(web3.utils.fromWei(result, "ether") + " ETH")
+        let theBalance = parseFloat(web3.utils.fromWei(result, 'ether')).toFixed(4);
+        setBalance(theBalance + " ETH")
       }
     })
   }
+
+  useEffect(() => {
+    setUpBalance()
+  }, [])
+
 
 
   // Create transaction
@@ -58,6 +65,8 @@ const Home: NextPage = () => {
       createTransaction.rawTransaction
     );
     setUpBalance()
+    setIsLoading(false)
+    setSuccess(true)
     console.log(
       `Transaction successful with hash: https://rinkeby.etherscan.io/tx/${createReceipt.transactionHash}`
     );
@@ -72,13 +81,14 @@ const Home: NextPage = () => {
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <p>{walletAddress}</p>
         <p>{balance}</p>
-        {!isLoading
+        {isLoading
           ? <Loader />
           : (
-            <button type="button" className="btnEth" onClick={sendEth}>Send 0.002 Eth </button>
+            <button type="button" style={{display: isSuccess ? 'none' : 'block',}} className="btnEth" onClick={sendEth}>Send 0.002 Eth </button>
           )}
-
+        {isSuccess && <p className='text-[green]'>Success!</p>}
       </main>
 
     </div>
